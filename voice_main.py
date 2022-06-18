8080c190b31508c16e7606432c1ff25878cbaacb
Jeff7531 <jeffmkwang@gmail.com>
2022-06-18 15:34:32 -0700

    Added new feature. TTS

--- voice_main.py
+++ voice_main.py
@@ -1,10 +1,13 @@
 import speech_recognition
 import pyttsx3
 import pyaudio
-
+engine = pyttsx3.init()
+engine.setProperty("rate",150)
 recognizer=speech_recognition.Recognizer()
 
-print("Hello! What is your name")
+engine.say("Hello! What is your name")
+print("Hello! What is your name?")
+engine.runAndWait()
 name=True
 while name:
     try:
@@ -15,6 +18,7 @@ while name:
             text=text.lower()
             text=text.split()
             if len(text) == 1:
+                engine.say(f"Is {text[0].capitalize()} your name?")
                 print(f"Is {text[0].capitalize()} your name?")
                 recognizer.adjust_for_ambient_noise(mic, duration=0.2)
                 audio=recognizer.listen(mic)
@@ -23,35 +27,46 @@ while name:
                 while repeat:
                     if answer.lower()=="yes":
                         print("Great lets continue")
+                        engine.say("Great lets continue")
                         repeat=False
                         name=False
                     elif answer.lower()=="no":
                         repeat=False
                         print("Lets try again")
                         print("What is your name")
+                        engine.say("Lets try again")
+                        engine.say("What is your name")
                     else:
                         print("Sorry. I didnt understand that.")
+                        engine.say("Sorry. I didnt understand that.")
             elif len(text) == 2:
                 repeat=True
                 while repeat:
                     print(f"Is {text[0].capitalize()} {text[1].capitalize()} your name?")
+                    engine.say(f"Is {text[0].capitalize()} {text[1].capitalize()} your name?")
                     recognizer.adjust_for_ambient_noise(mic, duration=0.2)
                     audio=recognizer.listen(mic)
                     answer=recognizer.recognize_google(audio)
                     if answer.lower()=="yes":
                         print("Great lets continue")
+                        engine.say("Great lets continue")
                         repeat=False
                         name=False
                     elif answer.lower()=="no":
                         repeat=False
                         print("Lets try again")
                         print("What is your name")
+                        engine.say("Lets try again")
+                        engine.say("What is your name")
                     else:
                         print("Sorry. I didnt understand that.")
+                        engine.say("Sorry. I didnt understand that.")
                 
             else:
                 print("Sorry I didnt get that.\nPlease try again.")
                 print("What is your name?")
+                engine.say("Sorry I didnt get that.\nPlease try again.")
+                engine.say("What is your name?")
     except:
         recognizer=speech_recognition.Recognizer()
         continue
